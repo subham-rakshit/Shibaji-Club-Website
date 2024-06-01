6cd8f3c7994fe07d20ff3e882c5f7ab5e01a46ce
subham-rakshit <subhamrakshit667@gmail.com>
2024-06-01 15:16:04 +0530

    Just check the VS Code source controll

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return <div className="text-red-500">Shibaji Sangha</div>;
 };
 
 export default App;
