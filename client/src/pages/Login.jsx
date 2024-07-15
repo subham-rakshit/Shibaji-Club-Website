d09e34335eca75344b01c186aaee38dfdbc6c6b5
subham-rakshit <subhamrakshit667@gmail.com>
2024-07-15 15:33:21 +0530

    change made in Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
 
 import { Button, Label, Spinner } from "flowbite-react";
 import { Input, OAuth } from "../components";
