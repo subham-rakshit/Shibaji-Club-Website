d057d6f75d36935bad79b3e28ab9fb95f162bd68
subham-rakshit <subhamrakshit667@gmail.com>
2024-07-22 22:44:40 +0530

    CORS updated

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const app = express();
 const corsOptions = {
   origin: "http://localhost:5173",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
+  credential: true,
 };
 
 app.use(cookieParser());
